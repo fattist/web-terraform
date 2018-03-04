d4047097c233c70a22cc6e1fe9cdf9b9781f285a
listenrightmeow <mike@fatt.ist>
2018-03-03 16:01:24 -0800

    redirect all sources to https

--- tf/modules/aws/cf/production/main.tf
+++ tf/modules/aws/cf/production/main.tf
@@ -21,9 +21,7 @@ resource "aws_cloudfront_distribution" "react-distribution" {
     custom_origin_config {
       http_port = 80
       https_port = 443
-      /*origin_keepalive_timeout = 60*/
-      origin_protocol_policy = "http-only"
-      /*origin_read_timeout = 60*/
+      origin_protocol_policy = "https-only"
       origin_ssl_protocols = ["TLSv1", "TLSv1.1", "TLSv1.2"]
     }
   }
@@ -65,7 +63,7 @@ resource "aws_cloudfront_distribution" "react-distribution" {
       }
     }
 
-    viewer_protocol_policy = "allow-all"
+    viewer_protocol_policy = "redirect-to-https"
     min_ttl = 14400
     default_ttl = 14400
     max_ttl = 14400
